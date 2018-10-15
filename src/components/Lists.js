76d042c422792928b88d0a99ccad1fdf1c0046d2
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2018-10-15 00:53:47 -0700

    changed styling

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Results from './Results';
-import { Header } from 'semantic-ui-react'
+
 
 const lists = {
-  margin: '20px 20px',
-  width: '40%',
-	backgroundColor: 'white',
+  margin: '30px 20px',
+  width: '50%',
+  backgroundColor: 'white',
+  position: 'absolute',
+  left: '50px'
 };
 
 
@@ -13,7 +15,7 @@ const Lists = props => {
   const ren = props.result.map(el => <Results key={el.id} {...el}/>);
   return (
     <div>
-      <div style={lists}>{ren}</div>;
+      <div style={lists}>{ren}</div>
     </div>
   );
 };
