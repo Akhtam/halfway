2029babc10c105fa0e5fb9c6a38eab881ab64270
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2018-10-16 21:45:32 -0700

    removed useless modules

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiKey = 'YELP_KEY';
-const yelp = require('yelp-fusion');
 const axios = require('axios');
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 
