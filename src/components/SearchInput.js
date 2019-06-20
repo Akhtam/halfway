18d81464dfffcfaa8981101e2e6d5cdaf3e5578c
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2019-06-19 18:27:34 -0700

    changed from class component to functional component using react hooks

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'semantic-ui-react';
 import keys from '../config/keys';
-import getMiddlePoint from '../helpers';
+import getMiddlePoint from '../helpers/midpointHelper';
 
 const btnStyle = {
   backgroundColor: '#479da0',
@@ -10,70 +10,62 @@ const btnStyle = {
 
 const url = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-class SearchInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputA: '',
-      inputB: ''
-    };
-  }
-
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+const SearchInput = ({ getResults }) => {
+  const [locationA, setInputA] = useState('');
+  const [locationB, setInputB] = useState('');
+  //handle submit
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const key = keys.API_KEY;
+    if (locationA.length > 5 && locationB.length > 5) {
+      const { calcMiddle, geoLocationA, geoLocationB } = await getMiddlePoint(
+        url,
+        key,
+        locationA,
+        locationB
+      );
+      const locA = {
+        geoLoc: geoLocationA,
+        fullAddress: locationA
+      };
+      const locB = {
+        geoLoc: geoLocationB,
+        fullAddress: locationB
+      };
+      console.log(calcMiddle);
+      getResults(calcMiddle, locA, locB);
+      setInputA('');
+      setInputB('');
+    } else {
+      alert('please enter Address');
+    }
   };
 
-  // handleSubmit = async e => {
-  //   e.preventDefault();
-  //   const { inputA, inputB } = this.state;
-  //   const key = keys.API_KEY;
-  //   if (inputA.length > 5 && inputB.length > 5) {
-  //     let m = await getMiddlePoint(url, key, inputA, inputB);
-  //     const mid = m.calcMiddle;
-  //     const locA = {
-  //       geoLoc: m.geoLocationA,
-  //       fullAddress: inputA
-  //     };
-  //     const locB = {
-  //       geoLoc: m.geoLocationB,
-  //       fullAddress: inputB
-  //     };
-  //     this.props.getResults(mid, locA, locB);
-  //     this.setState({ inputA: '', inputB: '' });
-  //   } else {
-  //     alert('please enter Address');
-  //   }
-  // };
-
-  render() {
-    return (
-      <div>
-        <Form>
-          <Form.Field>
-            <input     
-              value={this.state.inputA}
-              placeholder="Location A"
-              name='inputA'
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Form.Field>
-            <input     
-              value={this.state.inputB}
-              placeholder="Location B"
-              name='inputB'
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button type="submit" style={btnStyle} onClick={this.handleSubmit}>
-            Search
-          </Button>
-        </Form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Form>
+        <Form.Field>
+          <input
+            value={locationA}
+            placeholder="Location A"
+            name="inputA"
+            onChange={e => setInputA(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <input
+            value={locationB}
+            placeholder="Location B"
+            name="inputB"
+            onChange={e => setInputB(e.target.value)}
+          />
+        </Form.Field>
+        <Button type="submit" style={btnStyle} onClick={handleSubmit}>
+          Search
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default SearchInput;
