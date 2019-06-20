34df5159d615bb41859554bdf8659c6e4f9939ce
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2019-06-19 18:18:52 -0700

    ..

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const apiKey = '6hOZ3pLCSoj3EqjN5BiS_h9guA60T9WD6lN8D2q_5NzY-Y2hXiQEcaAMFEqySt0EFHr1pQtSBcJ0msNjysbquzU45w3GaV6HIfAMlPEZxk2ivDZOTQixj7hgqwATXHYx';
+const apiKey = '';
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
